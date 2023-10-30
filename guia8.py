8e39ebe1f5802b70604908f243e679b449157a65
carlbol <82982423+carlbol@users.noreply.github.com>
2023-10-30 17:24:58 -0300

    Update guia8.py

--- guia8.py
+++ guia8.py
@@ -48,6 +48,42 @@ def buscar_el_maximo(p:pila) -> int:
     return max(elementos)
 
 # Parte 3: Colas ...
+#Colas
+from queue import Queue as cola
+import random
+def generar_nros_al_azar(n:int, desde:int, hasta:int) -> cola:
+    cola_resultado:cola = cola()
+    for i in range(n):
+        entero_azaroso:int = random.randint(desde,hasta)
+        cola_resultado.put(entero_azaroso)
+    return cola_resultado
+
+
+def cantidad_elementos(c:cola) -> int:
+    contador:int = 0
+    c_auxiliar:cola = cola()
+    while not c.empty():
+        c_auxiliar.put(c.get())
+        contador += 1
+    while not c_auxiliar.empty():
+        c.put(c_auxiliar.get())
+    return contador
+
+cola_ejemplo = generar_nros_al_azar(10,1,10)
+#print(cantidad_elementos(cola_ejemplo))
+
+def buscar_el_maximo(c:cola) -> int:
+    elementos:[int] = []
+    c_auxiliar:cola = cola()
+
+    while not c.empty():
+        elemento:int = c.get()
+        elementos.append(elemento)
+        c_auxiliar.put(elemento)
+    while not c_auxiliar.empty():
+        elemento:int = c_auxiliar.get()
+        c.put(elemento)
+    return max(elementos)
 
 #Parte 4: Diccionarios
 
